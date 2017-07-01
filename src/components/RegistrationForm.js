95230b3f5d95077149baafec80fe794c6444cc8c
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-07-01 20:56:04 +0000

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class RegistrationForm extends React.Component {
+  render(){
+    return (
+      <form class="" action="index.html" method="post">
+        <input type="text" name="" value="" placeholder="name" />
+        <input type="password" name="" value="" placeholder="password" />
+        <button type="submit" name="" value="Register"/>
+      </form>
+    );
+  }
+}
+
+// export default RegistrationForm;
---
+++ src/components/Webpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import FillerText from './FillerText';
+
+class Webpage extends React.Component {
+  render(){
+    return (
+            <div>
+              <h2>The world's coolest webpage</h2>
+              <FillerText />
+              <FillerText />
+            </div>
+    );
+  }
+}
+export default Webpage;
